b6417df420119992d8c0bd52ed70bffbbee35bbe
Faye Purdum <fayepurdum@gmail.com>
2015-01-25 16:05:59 -0800

    fb og test

--- main.js
+++ main.js
@@ -20,6 +20,62 @@ SAS.main = (function() {
       });
     });
 
+    window.fbAsyncInit = function() {
+      var appId = "";
+      switch(window.location.host)
+      {
+      case "localhost:8081":
+        appId = "1422907571258597";
+        break;
+      case "localhost:8080":
+        appId = "305834246207937";
+        break;
+      case "livesite.com":
+        appId = "861782343846424";
+        break;
+      default:
+        alert('Could not find matching appId');
+      }
+
+      FB.init({
+        appId      : appId,                        // App ID from the app dashboard
+        channelUrl : '//'+window.location.host+'/channel.html', // Channel file for x-domain comms
+        status     : true,                                 // Check Facebook Login status
+        xfbml      : true                                  // Look for social plugins on the page
+      });
+
+      $('#facebook').on('click', function(e) {
+          e.preventDefault();
+
+          FB.ui(
+            {
+              method: 'feed',
+              name: 'Sharing on Facebook and you can too',
+              link: 'http://fayeishere.github.io/apiguide/',
+              picture: 'http://'+window.location.host+'/img/coffeecat.jpg',
+              caption: 'Caption is optional',
+              description: 'This is a fantastic description.'
+            },
+            function(response) {
+              if (response && response.post_id) {
+              } else {
+                
+              }
+            }
+          );
+      });
+
+    };
+
+    // Load the SDK asynchronously
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/all.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+
     return {
         initialize: initialize
     };
